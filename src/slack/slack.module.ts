0bb2377941ca6632d6a012b20bdaaa8439c37501
Dave Thomas <davet@ifit.com>
2022-05-24 14:32:46 -0700

    chore: update dependencies

--- src/slack/slack.module.ts
+++ src/slack/slack.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
 import { SlackService } from './services/slack';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
-  providers: [SlackService],
+  providers: [
+    SlackService,
+    ConfigService,
+    { provide: 'BOLT_APP', useValue: null },
+  ],
 })
 export class SlackModule {}
