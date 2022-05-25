e030b576e4c00fdc34021e4aec785385093e10a0
Dave Thomas <davet@ifit.com>
2022-05-25 15:00:10 -0700

    refactor: revise intent values

--- src/nlp/services/nlp.ts
+++ src/nlp/services/nlp.ts
@@ -1,52 +1,59 @@
 import { dockStart } from '@nlpjs/basic';
 import { Injectable, OnModuleInit } from '@nestjs/common';
 
+export enum Intent {
+  Greeting = 'GREETING',
+  OpenDoor = 'OPEN_DOOR',
+  CloseDoor = 'CLOSE_DOOR',
+  QueryState = 'QUERY_STATE',
+};
+
 const documents = [
-  ['hello', 'action.greeting'],
-  ['howdy', 'action.greeting'],
-  ['can you hear me', 'action.greeting'],
-  ['are you running', 'action.greeting'],
-  ['are you up', 'action.greeting'],
+  ['hello', Intent.Greeting],
+  ['howdy', Intent.Greeting],
+  ['can you hear me', Intent.Greeting],
+  ['are you running', Intent.Greeting],
+  ['are you up', Intent.Greeting],
 
-  ['open the garage door', 'action.garage.open'],
-  ['open the door', 'action.garage.open'],
-  ['open', 'action.garage.open'],
-  ['open up', 'action.garage.open'],
-  ['open it', 'action.garage.open'],
+  ['open the garage door', Intent.OpenDoor],
+  ['open the door', Intent.OpenDoor],
+  ['open', Intent.OpenDoor],
+  ['open up', Intent.OpenDoor],
+  ['open it', Intent.OpenDoor],
 
-  ['close the garage door', 'action.garage.close'],
-  ['close the door', 'action.garage.close'],
-  ['close', 'action.garage.close'],
-  ['close it', 'action.garage.close'],
-  ['shut the garage door', 'action.garage.close'],
-  ['shut the door', 'action.garage.close'],
-  ['shut', 'action.garage.close'],
-  ['shut it', 'action.garage.close'],
+  ['close the garage door', Intent.CloseDoor],
+  ['close the door', Intent.CloseDoor],
+  ['close', Intent.CloseDoor],
+  ['close it', Intent.CloseDoor],
+  ['shut the garage door', Intent.CloseDoor],
+  ['shut the door', Intent.CloseDoor],
+  ['shut', Intent.CloseDoor],
+  ['shut it', Intent.CloseDoor],
 
-  ['is it open', 'query.garage.state'],
-  ['garage door', 'query.garage.state'],
-  ['are you open', 'query.garage.state'],
-  ['state', 'query.garage.state'],
-  ['door state', 'query.garage.state'],
-  ["what's the door state", 'query.garage.state'],
+  ['is it open', Intent.QueryState],
+  ['garage door', Intent.QueryState],
+  ['are you open', Intent.QueryState],
+  ['state', Intent.QueryState],
+  ['door state', Intent.QueryState],
+  ["what's the door state", Intent.QueryState],
 ];
 
 const answers = [
-  ['action.greeting', 'Hello!'],
-  ['action.greeting', 'Hey there!'],
-  ['action.greeting', 'Greetings!'],
-  ['action.greeting', "I'm up & running."],
-  ['action.greeting', "I'm waiting for your commands."],
+  [Intent.Greeting, 'Hello!'],
+  [Intent.Greeting, 'Hey there!'],
+  [Intent.Greeting, 'Greetings!'],
+  [Intent.Greeting, "I'm up & running."],
+  [Intent.Greeting, "I'm waiting for your commands."],
 
-  ['action.garage.open', 'Ok, opening the garage door.'],
-  ['action.garage.open', 'Got it. Garage door opening.'],
-  ['action.garage.open', 'Opening garage door now.'],
-  ['action.garage.open', "I'm on it. Opening the door now."],
+  [Intent.OpenDoor, 'Ok, opening the garage door.'],
+  [Intent.OpenDoor, 'Got it. Garage door opening.'],
+  [Intent.OpenDoor, 'Opening garage door now.'],
+  [Intent.OpenDoor, "I'm on it. Opening the door now."],
 
-  ['action.garage.close', 'Ok, closing the garage door.'],
-  ['action.garage.close', 'Got it. Garage door closing.'],
-  ['action.garage.close', 'Closing garage door now.'],
-  ['action.garage.close', "I'm on it. Closing the door now."],
+  [Intent.CloseDoor, 'Ok, closing the garage door.'],
+  [Intent.CloseDoor, 'Got it. Garage door closing.'],
+  [Intent.CloseDoor, 'Closing garage door now.'],
+  [Intent.CloseDoor, "I'm on it. Closing the door now."],
 ];
 
 @Injectable()
