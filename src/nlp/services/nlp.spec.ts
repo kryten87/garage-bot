ee9a29d543490f2237ec990d872c1be024754a5c
Dave Thomas <davet@ifit.com>
2022-05-25 14:48:42 -0700

    chore: add specs for NlpService

---
+++ src/nlp/services/nlp.spec.ts
@@ -0,0 +1,30 @@
+import { NlpService } from './nlp';
+import { Test, TestingModule } from '@nestjs/testing';
+
+describe('Nlp', () => {
+  let provider: NlpService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [NlpService],
+    }).compile();
+
+    provider = module.get<NlpService>(NlpService);
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore calling private method; ok for testing
+    await provider.initialize();
+  });
+
+  it('should be defined', () => {
+    expect(provider).toBeDefined();
+  });
+
+  describe('process', () => {
+    it('should remap the results to the expected values', async () => {
+      const response = await provider.process('close the door');
+      expect(response.intent).toBe('action.garage.close');
+      expect(response.score).toBe(1);
+      expect(response.answer).not.toBeUndefined();
+    });
+  });
+});
