bc6de6e30bef1fb444ff128f8997f4f0c3f201cd
Dave Thomas <davet@ifit.com>
2022-05-25 09:03:11 -0700

    chore: add nlp module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
 import { SlackModule } from './slack/slack.module';
+import { NlpModule } from './nlp/nlp.module';
 
 @Module({
-  imports: [SlackModule, ConfigModule.forRoot()],
+  imports: [SlackModule, ConfigModule.forRoot(), NlpModule],
   controllers: [AppController],
   providers: [AppService],
 })
