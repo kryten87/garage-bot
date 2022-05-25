a97308abcac96d5d44bb3493625ccbb740f4bcb9
Dave Thomas <davet@ifit.com>
2022-05-25 15:00:23 -0700

    chore: implement nlp in controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 import { SlackService } from './slack/services/slack';
+import { NlpService } from './nlp/services/nlp';
 
 @Controller()
 export class AppController {
   constructor(
     private readonly appService: AppService,
     private readonly slackService: SlackService,
+    private readonly nlpService: NlpService
   ) {
     this.slackService.onMessage(/.*/, this.messageHandler);
   }
@@ -25,10 +27,15 @@ export class AppController {
     console.log(message);
     console.log('..........................');
     const { channel, ts } = message;
+    const { intent, score, answer } = await this.nlpService.process(message.text);
+    let text = answer;
+    if (!text || score < 0.75) {
+      text = 'I\'m not sure what you\'re saying. Can you try again?';
+    }
     await this.slackService.sendText({
       channel,
       thread: ts,
-      text: 'this should be a reply!',
+      text,
     });
   };
 }
