03b78a257e7d02ac252dba6602b51b4f1ad89574
Dave Thomas <davet@ifit.com>
2022-05-28 16:22:24 -0700

    chore: inject RPIO package

--- src/rpi/rpi.module.ts
+++ src/rpi/rpi.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { GpioService } from './services/gpio';
 import { ConfigService } from '@nestjs/config';
+import * as rpio from 'rpio';
 
 @Module({
   providers: [
     ConfigService,
     GpioService,
+    { provide: 'RPIO', useValue: rpio },
   ],
   exports: [GpioService],
 })
