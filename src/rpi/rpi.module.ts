dd11918079ffade55b98824746621aad1e85efb1
Dave Thomas <davet@ifit.com>
2022-05-28 16:18:18 -0700

    chore: add config service to rpi module

--- src/rpi/rpi.module.ts
+++ src/rpi/rpi.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { GpioService } from './services/gpio';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
-  providers: [GpioService],
+  providers: [
+    ConfigService,
+    GpioService,
+  ],
   exports: [GpioService],
 })
 export class RpiModule {}
