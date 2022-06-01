8d6f84549a61e74a8beb04d0b0441496f067c94f
Dave Thomas <davet@ifit.com>
2022-06-01 14:40:20 -0700

    chore: implement logging service

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
-import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core';
+import { SlackLogger } from './slack-logger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    bufferLogs: true,
+  });
+  app.useLogger(app.get(SlackLogger));
   await app.listen(3000);
 }
 bootstrap();
