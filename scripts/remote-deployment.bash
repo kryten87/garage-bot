3dd71d52cad8bcfcbf9d626ec368d74f2296566e
Dave Thomas <davet@ifit.com>
2022-06-05 17:04:31 -0700

    chore: update remote deployment script

--- scripts/remote-deployment.bash
+++ scripts/remote-deployment.bash
@@ -8,13 +8,16 @@ APP_DIR=homebot
 WORK_DIR=homebot-work
 STAGING_DIR=homebot-staging
 TEMP_DIR=homebot-temp
+DRIVER_DIR=gpio-driver
+DRIVER_STAGING_DIR=gpio-driver-temp
+DRIVER_TEMP_DIR=gpio-driver-temp
 
 echo "[deployment] change to location"
 cd /home/dave
 
 # remove the old directories
 echo "[deployment] clean up"
-rm -rf "$TEMP_DIR" "$WORK_DIR" "$STAGING_DIR"
+rm -rf "$TEMP_DIR" "$WORK_DIR" "$STAGING_DIR" "$DRIVER_TEMP_DIR"
 
 # clone the repository
 echo "[deployment] clone the repository"
@@ -22,6 +25,11 @@ git clone git@gitlab.com:dave137/home-bot.git "$WORK_DIR"
 
 echo "[deployment] enter the directory"
 cd "$WORK_DIR"
+
+# copy the GPIO driver dir
+#
+echo "[deployment] install gpio driver"
+cp -Rp gpio-driver "/home/dave/$DRIVER_STAGING_DIR"
 
 # install the dependencies
 echo "[deployment] install dependencies"
@@ -47,19 +55,29 @@ if [ -e "/home/dave/$APP_DIR" ]; then
   cp "/home/dave/$APP_DIR/.env" "/home/dave/$STAGING_DIR/.env"
 fi
 
-# stop the service
-echo "[deployment] stop service"
+# stop the services
+echo "[deployment] stop services"
 sudo systemctl stop homebot
+sudo systemctl stop gpio-driver
 sleep 10s
 
-echo "[deployment] replace service"
+echo "[deployment] removing pipes"
+rm /tmp/gpio_driver_*
+
+echo "[deployment] replace homebot service"
 if [ -e "/home/dave/$APP_DIR" ]; then
   mv "/home/dave/$APP_DIR" "/home/dave/$TEMP_DIR"
 fi
 mv "/home/dave/$STAGING_DIR" "/home/dave/$APP_DIR"
 
+echo "[deployment] replace driver"
+if [ -e "/home/dave/$DRIVER_DIR" ]; then
+  mv "/home/dave/$DRIVER_DIR" "/home/dave/$DRIVER_TEMP_DIR"
+fi
+mv "/home/dave/$DRIVER_STAGING_DIR" "/home/dave/$DRIVER_DIR"
+
 echo "[deployment] start service"
 sudo systemctl start homebot
 
 echo "[deployment] final clean up"
-rm -rf "$TEMP_DIR" "$WORK_DIR" "$STAGING_DIR"
+rm -rf "$TEMP_DIR" "$WORK_DIR" "$STAGING_DIR" "$DRIVER_TEMP_DIR" "$DRIVER_STAGING_DIR"
